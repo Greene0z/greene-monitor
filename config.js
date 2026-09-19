6922d8026968503a7e503e8513e92438062288a5
DabliuM <161660922+Greene0z@users.noreply.github.com>
2026-09-18 21:05:36 -0300

    Update Supabase configuration values

--- config.js
+++ config.js
@@ -1,2 +1,2 @@
 // IMPORTANTE: se você já usa a V1, mantenha o config.js que está atualmente no seu GitHub.
-window.GREENE_CONFIG={SUPABASE_URL:"COLE_SUA_PROJECT_URL_AQUI",SUPABASE_ANON_KEY:"COLE_SUA_PUBLISHABLE_KEY_AQUI"};
+window.GREENE_CONFIG={SUPABASE_URL:"https://mzdwkmpqxmmozmhaojge.supabase.co",SUPABASE_ANON_KEY:"sb_publishable_pHAGi_HzFPE1Md-UoLcjZA_zjbwzX__"};
